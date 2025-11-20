8032151b409083a85aaf67a4a4de174bad5b39b1
mk09777777 <153955758+mk09777777@users.noreply.github.com>
2025-11-20 23:09:59 +0530

    edited code

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,9 +34,9 @@ const SearchBar = ({ onSearch, placeholder = "Search cars..." }) => {
                             ✕
                         </button>
                     )}
-                    <div className="px-3 py-1 text-gray-400">
-                        🔍
-                    </div>
+                     <svg className="w-5 h-5 text-gray-400 mr-3" fill="none" stroke="currentColor" viewBox="0 0 22 22">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
                 </div>
             </div>
         </div>
--- src/pages/CarsPage.jsx
+++ src/pages/CarsPage.jsx
@@ -16,7 +16,7 @@ export default function CarsPage() {
         <div className="min-h-screen bg-gray-50">
             <Navigation headerColor={"#414141"} />
             <div className="container mx-auto px-4 py-8">
-                <h1 className="text-3xl font-bold text-center mb-8">Our Car Collection</h1>
+          
                 <SearchBar onSearch={handleSearch} placeholder="Search by name, brand, model, or type..." />
                 <CarCard fetchFunction={fetchAllCars} />
             </div>
